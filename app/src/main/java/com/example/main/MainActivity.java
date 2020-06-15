b4ae488b42a861a55ad74e7c9c15013b9149d6ac
minsung <qww1552@naver.com>
2020-06-15 21:53:45 +0900

    spinner

--- app/src/main/java/com/example/main/MainActivity.java
+++ app/src/main/java/com/example/main/MainActivity.java
@@ -5,7 +5,11 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.Spinner;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -16,6 +20,30 @@ public class MainActivity extends AppCompatActivity {
 
         Button startBtn = (Button)findViewById(R.id.startBtn);
         Button button = findViewById(R.id.button);
+
+        Spinner spinner = findViewById(R.id.spinner);
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this, R.array.city_array, android.R.layout.simple_spinner_item);
+        // Specify the layout to use when the list of choices appears
+
+        // Apply the adapter to the spinner
+        spinner.setAdapter(adapter);
+        spinner.setPrompt("지역선택");
+        spinner.setEnabled(true);
+
+
+        spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                String imsi = parent.getItemAtPosition(position).toString();
+                Toast.makeText(getApplicationContext(),imsi,Toast.LENGTH_LONG).show();
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        });
+
         startBtn.setOnClickListener(new Button.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -23,7 +51,6 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
-
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
